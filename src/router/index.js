f67c4ba52a13dda45b00dc6011171e28f4574a03
emfo2405 <emfo2405@student.miun.se>
2026-01-07 13:55:57 +0100

    Routes tillagda för alla views

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import startView from '@/views/startView.vue'
+import addView from '@/views/addView.vue'
+import categoriesView from '@/views/categoriesView.vue'
+import updateView from '@/views/updateView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: "/",
+    name: "start",
+    component: startView
+  },
+  {
+    path: "/categories",
+    name: "categories",
+    component: categoriesView
+  }, 
+  {
+    path: "/add",
+    name: "add",
+    component: addView
+  },
+  {
+    path: "/update",
+    name: "update",
+    component: updateView
+  }
+  ],
+
+
 })
 
 export default router
