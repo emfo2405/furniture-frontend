1e1ec99930d3a6a458da93d3ff610c6d184e53d7
emfo2405 <emfo2405@student.miun.se>
2026-01-07 16:21:47 +0100

    Formulär tillagt på startsidan och bootstrap design

--- src/views/startView.vue
+++ src/views/startView.vue
@@ -12,4 +12,20 @@
         </p>
     </div>
 
+    <div id="signIn" class="d-flex flex-column align-items-center">
+        <form class="p-3 w-50  border border-dark rounded">
+            <label for="email" class="form-label">E-post:</label>
+            <input type="email" class="form-control" id="email">
+
+            <label for="password" class="form-label">Lösenord:</label>
+            <input type="password" class="form-control" id="password">
+
+            <button id="logInButton" type="submit" class="btn btn-secondary mt-4">Logga in</button>
+
+            
+        </form>
+
+        <p>Har du inget konto än? Skapa ett <a>här</a></p>
+    </div>
+
 </template>
