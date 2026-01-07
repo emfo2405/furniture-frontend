fb8da50c0311960080049727c6e499dc6cf449cd
emfo2405 <emfo2405@student.miun.se>
2026-01-07 14:20:39 +0100

    Meny skapad i Header.vue och testinnehåll tillagt i views

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <div id="menu">
+            <h1>FurnitureStore</h1>
+            <nav>
+                <ul>
+                    <li><RouterLink to="/">Start</RouterLink></li>
+                    <li><RouterLink to="/categories">Kategorier</RouterLink></li>
+                    <li><RouterLink to="/add">Lägg till</RouterLink></li>
+                    <li><RouterLink to="/update">Uppdatera</RouterLink></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+
+<script>
+    import { RouterLink } from 'vue-router';
+</script>
