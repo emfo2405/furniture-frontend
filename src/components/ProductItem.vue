b4aa9ae944b09bf02297598eea39c8d0072e8097
emfo2405 <emfo2405@student.miun.se>
2026-01-08 10:29:04 +0100

    En komponent skapad för att visa produkter

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <article class="d-flex flex-column align-items-center">
+        <div class="categoryItem border border-dark m-4 p-3 w-50 d-flex flex-column align-items-center">
+            <h3>{{ product.name }}</h3>
+            <p> {{ product.description }}</p>
+        </div>
+    </article>
+</template>
+
+<script setup>
+    const props = defineProps({
+        product: Object
+    })
+</script>
