33b7085d668aea5de52ebea98050304a01aec9e4
emfo2405 <emfo2405@student.miun.se>
2026-01-08 08:51:27 +0100

    Funktion för att läsa in data skapad

--- src/views/categoriesView.vue
+++ src/views/categoriesView.vue
@@ -1,3 +1,26 @@
 <template>
 <h1>Categories!</h1>
 </template>
+
+<script>
+    const getCategories = async() =>{
+
+    try {
+
+        const res = await fetch("https://furniture-backend-aym8.onrender.com/categories")
+
+        if(res.ok) {
+            const data = await res.json();
+
+        } else {
+
+        }
+
+    } catch (error){
+        console.log("There was an error: " + error)
+    } finally {
+        
+    }
+    }
+
+</script>
