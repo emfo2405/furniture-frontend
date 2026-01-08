d1806a054c9b725cd69b20d7ddb62550c66f6431
emfo2405 <emfo2405@student.miun.se>
2026-01-08 17:39:47 +0100

    Formulär tillagt för att lägga till en kategori

--- src/views/addView.vue
+++ src/views/addView.vue
@@ -1,3 +1,19 @@
 <template>
-<h1>Add!</h1>
+    <div id="categoryForm">
+        <form>
+<h2>Lägg till en kategori</h2>
+
+    <label for="name" class="form-label">Namn:</label>
+    <input type="name" class="form-control" id="name">
+
+    <label for="beskrivning" class="form-label">Beskrivning:</label>
+    <input type="beskrivning" class="form-control" id="description">
+
+    <button id="addButton" type="submit" class="btn btn-secondary mt-4">Lägg till</button>
+</form>
+</div>
 </template>
+
+<script setup>
+
+</script>    
