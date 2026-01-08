3f93c468edac7719da9b0ba32bdf96fd7c8ae3b9
emfo2405 <emfo2405@student.miun.se>
2026-01-08 09:52:46 +0100

    Text om att kategorier laddas in tillagd

--- src/views/categoriesView.vue
+++ src/views/categoriesView.vue
@@ -6,12 +6,14 @@ import CategoryItem from '@/components/CategoryItem.vue';
 
 
     const categories = ref([])
+    const loading = ref(false)
 
     onMounted(() => {
         getCategories();
     })
 
     const getCategories = async() =>{
+        loading.value = true;
 
     try {
 
@@ -27,7 +29,7 @@ import CategoryItem from '@/components/CategoryItem.vue';
     } catch (error){
         console.log("There was an error: " + error)
     } finally {
-
+        loading.value = false;
     }
     }
 
@@ -35,5 +37,6 @@ import CategoryItem from '@/components/CategoryItem.vue';
 
 <template>
         <h2 class="m-3 text-center">Våra kategorier</h2>
+        <p v-if="loading" class="loading text-center text-muted">Kategorier laddas in...</p>
 <CategoryItem v-for="category in categories" :category="category" :key="category._id"/>
 </template>
